732b8463d8de9a931265b9ac6779570bbb0ee4f7
BuildTools <unconfigured@null.spigotmc.org>
2024-12-31 19:50:33 +0800

    更新自然音源

--- senivatts.py
+++ senivatts.py
@@ -4,7 +4,7 @@ def senivatts(text, lang='ka', voice=1, rate=1, file=None, output=None):
         dict = yaml.safe_load(s)
     info = dict[lang]
 
-    if file != None:
+    if file is not None:
         with open(file,'r',-1,'utf-8') as f:
             text = f.read()
     from unidecode import unidecode
@@ -12,12 +12,13 @@ def senivatts(text, lang='ka', voice=1, rate=1, file=None, output=None):
     import re
     for rule in info['rules']:
         text = re.sub(rule, info['rules'][rule], text)
+    text = re.sub(':', '-', text)
 
     import pyttsx4
     engine = pyttsx4.init('sapi5')
     engine.setProperty('voice', r'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices' + '\\' + info['voices'][voice-1])
     engine.setProperty('rate', info['rates'][voice-1] * rate)
-    if output == None:
+    if output is None:
         engine.say(text)
     else:
         engine.save_to_file(text, output)
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--file", help="读取文件")
     parser.add_argument("-t", "--text", help="读取文本，默认nes!, basone", default='nes!, basone')
     parser.add_argument("-l", "--lang", help="音源语种，默认ka", default='ka')
-    parser.add_argument("-v", "--voice", help="音源编号，默认0", type=int, default=0)
+    parser.add_argument("-v", "--voice", help="音源编号，默认1", type=int, default=1)
     parser.add_argument("-r", "--rate", help="速度倍率，默认1", type=float, default=1)
     parser.add_argument("-o", "--output", help="输出音频文件")
     args = parser.parse_args()
